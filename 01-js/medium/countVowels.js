b90283ad99b53b9771532e830eabb151bd4cb992
nb-prog01 <nirajbhadoria007@gmail.com>
2024-01-31 17:13:33 +0530

    findLargestElement and countVowels code added

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,36 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  //using for loops
+
+    let count=0;
+    let vowels=['a','e','i','o','u'];
+    str=str.toLowerCase();
+    strArray=str.split("");
+
+  for(let i=0;i<strArray.length;i++){
+    vowels.forEach((element)=>{if(element==strArray[i]){
+      count+=1;
+    }})
+  }
+  console.log(count)
+  return count;
+
+  //using regex
+
+  // count=str.match(/[aeiou]/gi);
+  // console.log(count)
+  // if (count==null){
+  //   return 0;
+  // }else{
+  //   return count.length;
+  // }
+
+  
+
 }
+// countVowels("qaedcfgtyhjiopwcncpa adsadiao")
+// countVowels("");
+// countVowels("kjhlgf");
 
 module.exports = countVowels;
