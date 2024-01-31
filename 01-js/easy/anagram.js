bc6896878a37e5cffbb27baccfba961dd7e028ff
nb-prog01 <nirajbhadoria007@gmail.com>
2024-01-31 14:40:33 +0530

    Aanagram code added

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,37 @@
 */
 
 function isAnagram(str1, str2) {
+  let a=str1.toLowerCase().split("");
+  let b=str2.toLowerCase().split("");
+
+  a=a.sort();
+  b=b.sort();
+
+  //using for loop
+
+  // if(a.length!==b.length)
+  //   {return false;}
+  //     else{
+  //       for(var i=0;i<a.length;i++){
+  //         if(a[i]!==b[i]){
+  //           return false;
+  //         }
+  //       }
+  //     return true;
+  // } 
+
+  // using every
+
+  if(a.length === b.length && a.every((element, index) => element === b[index])){
+    return true;
+  }else{
+    return false;
+  }
 
 }
+
+//  let str1='Debit Card';
+//  let str2='Bad Credit';
+//  isAnagram(str1,str2);
 
 module.exports = isAnagram;
